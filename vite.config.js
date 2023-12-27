f1df6cf26a722ae5603df00f1c79ca7d595efc32
bdc4 <17170670+bdc4@users.noreply.github.com>
2023-12-27 13:51:01 -0800

    added deployment script

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  exports: {
+    publicPath: '/connections/'
+  },
   plugins: [
     vue({
       template: { transformAssetUrls }
