64ead6f2c5ea8bab25686dc57d9849c7e225436e
bdc4 <17170670+bdc4@users.noreply.github.com>
2024-01-24 21:27:30 -0800

    removed star eyes

--- src/components/ConnectionsTracker.vue
+++ src/components/ConnectionsTracker.vue
@@ -4,9 +4,9 @@
       <div v-for="(row, $ind) in tracker" :key="$ind">
         <span v-for="(emoji, $$ind) in row" :key="$$ind" v-html="emoji"></span>
       </div>
-      <div v-if="tracker.length == 4 && !hintsUsed">
+      <!--div v-if="tracker.length == 4 && !hintsUsed">
         <span>🤩🤩🤩🤩</span>
-      </div>
+      </div-->
     </div>
   </div>
 </template>
@@ -24,6 +24,9 @@ export default {
       });
 
       return used;
+    },
+    star () {
+      return (this.tracker.length == 4 && !this.hintsUsed) ? '⭐️' : '';
     }
   }
 }
